b96af8f0da08703e135241985fa3a3ee30e4153c
gabrielkipper1 <g.kipper@hotmail.com>
2024-03-09 17:38:32 -0300

    create animation interface to add custom animations

---
+++ Assets/Scripts/Animations/Animations.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animations
+{
+    public static void AnimatePositionAndScale(GameObject gameObject, Vector3 position, Vector3 scale, float time, bool autoStart = true)
+    {
+        PositionAndScaleAnimation animation = gameObject.AddComponent<PositionAndScaleAnimation>();
+        animation.SetParameters(position, scale, time);
+
+        if (autoStart)
+        {
+            animation.Animate();
+        }
+    }
+}
---
+++ Assets/Scripts/Animations/PositionAndScaleAnimation.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PositionAndScaleAnimation : MonoBehaviour, IAnimation
+{
+    public Vector3 position;
+    public Vector3 scale;
+    public float time;
+
+    public void SetParameters(Vector3 position, Vector3 scale, float time)
+    {
+        this.position = position;
+        this.scale = scale;
+        this.time = time;
+    }
+
+    public void Animate()
+    {
+        //animate
+        StartCoroutine(animatePositionAndScale());
+    }
+
+    IEnumerator animatePositionAndScale()
+    {
+        Debug.Log("Iniciando animacao");
+        float timer = time;
+        while(timer > 0)
+        {
+            gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, position, 10* time * Time.deltaTime);
+            gameObject.transform.localScale = Vector3.MoveTowards(gameObject.transform.lossyScale, scale, 10* time * Time.deltaTime);
+            timer -= Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
