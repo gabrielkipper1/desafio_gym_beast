f02b5659887fe89fa897924558d41f67152fc6ac
Gabriel Limberger <g.kipper@hotmail.com>
2024-03-10 12:34:15 -0300

    add OnStackUpdated event

--- Assets/Scripts/Character/Stack/CharacterStack.cs
+++ Assets/Scripts/Character/Stack/CharacterStack.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CharacterStack : MonoBehaviour
 {
+    public UnityEvent<int> OnStackUpdated = new UnityEvent<int>();
     public IAnimation removeFromStackAnimation;
     public IAnimation addToStackAnimation;
 
