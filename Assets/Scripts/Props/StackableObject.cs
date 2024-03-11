bf1b3a08f0137722127218d1cfc89257d8b1c797
Gabriel Limberger <g.kipper@hotmail.com>
2024-03-11 18:15:37 -0300

    make stackable objects use events, has vfx, and implement common functions

--- Assets/Scripts/Props/StackableObject.cs
+++ Assets/Scripts/Props/StackableObject.cs
@@ -1,11 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class StackableObject : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject staticObject;
+    public UnityEvent<StackableObject> OnPickedUp = new UnityEvent<StackableObject>();
+    public UnityEvent<StackableObject> OnDropped = new UnityEvent<StackableObject>();
+
+    public Transform root;
     public bool pickedUp;
     public Vector3 offset;
+
+    [SerializeField]
+    private GameObject onDropFx;
+
+    void Start()
+    {
+        if (root == null)
+        {
+            root = transform;
+        }
+    }
+
+    public void MarkAsPickedUp()
+    {
+        pickedUp = true;
+        OnPickedUp.Invoke(this);
+    }
+
+    public void DropAndDestroy()
+    {
+        Instantiate(onDropFx, transform.position, Quaternion.identity);
+        OnDropped.Invoke(this);
+        Destroy(gameObject);
+    }
 }
